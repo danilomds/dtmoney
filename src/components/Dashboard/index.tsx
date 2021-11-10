2472eb12e99c393b4009e06087bfcd8d7e69a7c0
Danilo Mendes <danilomds2007@gmail.com>
2021-11-10 15:12:47 -0300

    starting new project

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,12 @@
+import * as S from './styles';
+
+import { Summary } from '../Summary';
+
+export function Dashboard() {
+  return (
+    <S.Container>
+      <Summary />
+    </S.Container>
+    
+  )
+}
---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,56 @@
+import { useEffect } from 'react';
+import { api } from '../../services/api';
+import * as S from './styles';
+
+export function TransactionsTable() {
+  useEffect(() => {
+    api.get('transactions')
+    .then(response => console.log(response.data))
+  }, []);
+
+  return (
+    <S.Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Desenvolvimento de website</td>
+            <td className="deposit">R$12.000,00</td>
+            <td>Desenvolvimento</td>
+            <td>01/11/2021</td>
+          </tr>
+
+          <tr>
+            <td>Desenvolvimento de website</td>
+            <td className="deposit">R$12.000,00</td>
+            <td>Desenvolvimento</td>
+            <td>06/11/2021</td>
+          </tr>
+
+          <tr>
+            <td>Desenvolvimento de website</td>
+            <td className="deposit">R$12.000,00</td>
+            <td>Desenvolvimento</td>
+            <td>08/11/2021</td>
+          </tr>
+
+          <tr>
+            <td>Aluguel</td>
+            <td className="withdraw">- R$16.000,00</td>
+            <td>Casa</td>
+            <td>10/11/2021</td>
+          </tr>
+        </tbody>
+      </table>
+    </S.Container>
+
+  )
+}
